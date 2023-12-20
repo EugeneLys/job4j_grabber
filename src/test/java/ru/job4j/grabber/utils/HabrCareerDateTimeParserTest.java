f88d53fa4c606e6d165dcb77d24a6a86026a7521
EugeneLys <eugene.lysakov@gmail.com>
2023-12-20 20:18:14 +0500

    2.1. Преобразование даты [#289476]

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void whenCorrectParcingManually() {
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime result = parser.parse("2023-12-20T10:15:30+03:00");
+        LocalDateTime expected = LocalDateTime.parse("2023-12-20T10:15:30");
+        assertEquals(expected, result);
+    }
+}
